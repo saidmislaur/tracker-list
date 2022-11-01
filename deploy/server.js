c0816ead1c8c30b10b43cbd4cf39cef15676e0ec
saidmislaur <saidmislaur@gmail.com>
2022-11-01 22:45:41 +0300

    Deploy

--- deploy/server.js
+++ deploy/server.js
@@ -5,8 +5,8 @@ import { fileURLToPath } from 'url';
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const publicPath = path.join(__dirname, '..', 'public');
+const dirname = path.dirname(__filename);
+const publicPath = path.join(dirname, '..', 'public');
 const port = 3000;
 
 app.use(express.static(publicPath));
